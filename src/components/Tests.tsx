954c0e79c5c7044fdc76b6308d19ca1b816e4bb9
Eugene Kosukhin <kosuha606@gmail.com>
2023-02-25 18:05:55 +0300

    alg data types

--- src/components/Tests.tsx
+++ src/components/Tests.tsx
@@ -1,24 +1,32 @@
 import * as R from 'ramda'
 import {IO} from "../core/containers";
-import {user} from "../state";
+import {ChangeEvent, useState} from "react";
 
 const {log} = console
 
-const perform = (io: any) => io.unsafePerformIO ? io.unsafePerformIO() : null
+const performIO = (io: any) => io.unsafePerformIO ? io.unsafePerformIO() : null
 // myLogger - это чистая функция, нечистая - анонимная функция
 const myLogger = (x: any) => new IO(() => console.log(x))
 // Покажет имя юзера в консоль
-const showName = R.compose(perform, myLogger, R.prop('name'))
+const showName = R.compose(myLogger, R.prop('name'))
 const composeTest = R.compose(() => log('two'), () => log('one'))
 
-user.name = 'Eugene';
-
 function Tests() {
-    const toUpper = R.map(R.toUpper)
-    const onShowName = () => showName(user)
+    const [user, setUser] = useState({
+        name: 'Ivan',
+    });
+    const onShowName = () => showName(user).unsafePerformIO()
+    const changeName = (event: ChangeEvent<HTMLInputElement>) =>  {
+        setUser({name: event.target.value})
+    }
 
     return (
         <div>
+            <input
+                className="peer block min-h-[auto] w-full rounded border-1 bg-transparent py-[0.32rem] px-3 leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+                value={user.name}
+                onChange={changeName}
+            />
             <button onClick={onShowName}>show name</button>
         </div>
     )
